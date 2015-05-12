2f7543e4c66992c7410bd9c75e1cf5aded93b89c
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2015-05-11 23:51:06 -0300

    initial commit

---
+++ readme.markdown
@@ -0,0 +1,33 @@
+# rowboat
+
+> Rebundle unpacked Browserify bundles with optional mockups
+
+# Install
+
+Get it from `npm`
+
+```bash
+npm install rowboat --save
+```
+
+# Usage
+
+```js
+var unpack = require('browser-unpack');
+var rowboat = require('rowboat');
+var rows = unpack(bundle);
+var main = rowboat(rows);
+```
+
+# `rowboat(rows, mockups?)`
+
+Takes the `rows` output from [browser-unpack][1] and leverages [proxyquire][2] to rebundle the fragmented Browserify bundle. No external dependencies are necessary, and you can in fact provide `mockups` for dependencies you need to mock.
+
+This makes `rowboat` ideal in testing scenarios where you need to assert whether a bundle conforms to certain specifications without necessarily having to look at the actual source code that was used to compile said bundle.
+
+# License
+
+MIT
+
+[1]: https://github.com/substack/browser-unpack
+[2]: https://github.com/thlorenz/proxyquire
---
+++ rowboat.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs');
+var tmp = require('tmp');
+var proxyquire = require('proxyquire');
+
+function rowboat (rows, mocks) {
+  var entry = _.find(rows, 'entry');
+  var mockups = _.keys(mocks || {}).reduce(noCall, {});
+  return depTree(entry, mockups);
+
+  function noCall (accumulator, key) {
+    accumulator[key] = mocks[key];
+    accumulator[key]['@noCallThru'] = true;
+    return accumulator;
+  }
+
+  function dep (accumulator, key) {
+    if (key in accumulator) {
+      return accumulator;
+    }
+    var dependency = _.find(rows, { id: entry.deps[key] });
+    accumulator[key] = depTree(dependency, {});
+    accumulator[key]['@noCallThru'] = true;
+    return accumulator;
+  }
+
+  function depTree (row, dependencies) {
+    var map = _.keys(row.deps).reduce(dep, dependencies);
+    var file = tmp.tmpNameSync();
+    fs.writeFileSync(file, entry.source, 'utf8');
+    return proxyquire(file, map);
+  }
+}
+
+module.exports = rowboat;
